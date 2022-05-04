e4b2b1477e99dd73c1a82af69ad944fe5b9b8683
Denismay Concepcion Rosa <denismaycr@gmail.com>
2022-05-04 08:51:09 -0400

    0104052022

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  important: true,
+  content: [],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}'],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
